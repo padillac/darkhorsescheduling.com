527379802f70f1ae8af94026038a1350bb00aac1
padillac <shinimaninima@gmail.com>
2019-02-22 17:35:12 -0600

    Added font

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
   <title>Dark Horse Scheduling</title>
   <link rel="stylesheet" href="/static/main.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik">
   <meta description="Hosted schedule management solution for equine facilities">
 </head>
 
