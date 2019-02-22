2f744e16392bc6ef963e4007925cda2f8cd7ce08
padillac <shinimaninima@gmail.com>
2019-02-22 17:12:05 -0600

    First commit, with index.php and main css page

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+  <title>Dark Horse Scheduling</title>
+  <link rel="stylesheet" href="/static/main.css">
+  <meta description="Hosted schedule management solution for equine facilities">
+</head>
+
+<body>
+  <h1>Dark Horse Scheduling</h1>
+
+
+
+</body>
+
+</html>
