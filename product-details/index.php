ef0001d3fba2fbc2273688c69777101f089ecf3a
padillac <shinimaninima@gmail.com>
2019-07-24 17:12:24 -0500

    adding some content to product details page

--- product-details/index.php
+++ product-details/index.php
@@ -16,11 +16,17 @@
 
   <div class="main-content">
     <h2>Personalized Daily Schedules</h2>
+    <p>blablablablablablablablablablablablablablabla</p>
     <h2>Hour Tracking</h2>
+    <p>blablablablablablablablablablablablablablabla</p>
     <h2>Smart Scheduling with Conflict Detection</h2>
+    <p>blablablablablablablablablablablablablablabla</p>
     <h3>Horse Use Limits</h3>
+    <p>blablablablablablablablablablablablablablabla</p>
     <h2>Organization Directories</h2>
+    <p>blablablablablablablablablablablablablablabla</p>
     <h2>All your data, in one place</h2>
+    <p>blablablablablablablablablablablablablablabla</p>
   </div>
 
 </body>
