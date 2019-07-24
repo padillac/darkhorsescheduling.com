42d226ad8a7bbc84b5f672e335fe54a833e0465b
padillac <shinimaninima@gmail.com>
2019-07-24 18:00:09 -0500

    adding content to product details

--- product-details/index.php
+++ product-details/index.php
@@ -16,17 +16,17 @@
 
   <div class="main-content">
     <h2 style="text-align: right;">Personalized Daily Schedules</h2>
-    <p style="text-align: right;">blablablablablablablablablablablablablablabla</p>
+    <p style="text-align: right;">Staff, volunteers, and clients can simply type in their name to view their daily, personalized schedules. Avoid confusion</p>
     <h2>Hour Tracking</h2>
-    <p>blablablablablablablablablablablablablablabla</p>
+    <p>DHS gives staff and volunteers an easy way to record their work hours, anytime, from anywhere. </p>
     <h2 style="text-align: right;">Smart Scheduling with Conflict Detection</h2>
-    <p style="text-align: right;">blablablablablablablablablablablablablablabla</p>
+    <p style="text-align: right;">Whenever a change to the schedule is made, DHS automatically checks every person, horse, and resource against the database to ensure that nothing is double-booked.</p>
     <h3 style="text-align: right;">Horse Use Limits</h3>
-    <p style="text-align: right;">blablablablablablablablablablablablablablabla</p>
+    <p style="text-align: right;">Set daily and weekly work limits for each horse, DHS will help ensure that no horse is overworked</p>
     <h2>Organization Directories</h2>
-    <p>blablablablablablablablablablablablablablabla</p>
+    <p>Keep all organization contacts in one place</p>
     <h2 style="text-align: right;">All your data, in one place</h2>
-    <p style="text-align: right;">blablablablablablablablablablablablablablabla</p>
+    <p style="text-align: right;">Administrators have easy access to all organization information. Download reports of volunteer and staff hours, class information, and volunteer shifts.</p>
   </div>
 
 </body>
