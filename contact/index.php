61ea9cac0c70f2f5a174ad64557e1376bdd02cc3
padillac <shinimaninima@gmail.com>
2019-07-24 18:31:15 -0500

    updating contact me page

--- contact/index.php
+++ contact/index.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <title>Contact | Dark Horse Scheduling</title>
   <link rel="stylesheet" href="/static/main.css">
+  <link rel="stylesheet" href="/static/contact-page.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans"/>
   <meta description="Hosted schedule management solution for equine facilities">
