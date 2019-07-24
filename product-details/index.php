854576888246f87de87a41ab8a78b7f0baa9f39a
padillac <shinimaninima@gmail.com>
2019-07-24 18:08:56 -0500

    adding content to product details

--- product-details/index.php
+++ product-details/index.php
@@ -15,18 +15,18 @@
   <?php include $_SERVER['DOCUMENT_ROOT'] . "/static/site-header.html"; ?>
 
   <div class="main-content">
-    <h2 style="text-align: right;">Personalized Daily Schedules</h2>
-    <p style="text-align: right;">Staff, volunteers, and clients can simply type in their name to view their daily, personalized schedules. DHS helps avoid confusion and an excess of information, to make sure that everyone knows where they need to be.</p>
+    <h2 style="text-align: right; right: 40%;">Personalized Daily Schedules</h2>
+    <p style="text-align: right; right: 40%;">Staff, volunteers, and clients can simply type in their name to view their daily, personalized schedules. DHS helps avoid confusion and an excess of information, to make sure that everyone knows where they need to be.</p>
     <h2>Hour Tracking</h2>
     <p>DHS gives staff and volunteers an easy way to record their work hours, anytime, from anywhere. </p>
-    <h2 style="text-align: right;">Smart Scheduling with Conflict Detection</h2>
-    <p style="text-align: right;">Whenever a change to the schedule is made, DHS automatically checks every person, horse, and resource against the database to ensure that nothing is double-booked.</p>
-    <h3 style="text-align: right;">Horse Use Limits</h3>
-    <p style="text-align: right;">Set daily and weekly work limits for each horse, DHS will help ensure that no horse is overworked</p>
+    <h2 style="text-align: right; right: 40%;">Smart Scheduling with Conflict Detection</h2>
+    <p style="text-align: right; right: 40%;">Whenever a change to the schedule is made, DHS automatically checks every person, horse, and resource against the database to ensure that nothing is double-booked.</p>
+    <h3 style="text-align: right; right: 40%;">Horse Use Limits</h3>
+    <p style="text-align: right; right: 40%;">Set daily and weekly work limits for each horse, DHS will help ensure that no horse is overworked</p>
     <h2>Organization Directories</h2>
     <p>Keep all organization contacts together, and easily accessible.</p>
-    <h2 style="text-align: right;">All your data, in one place</h2>
-    <p style="text-align: right;">Administrators have easy access to all organization information. Download CSV reports of volunteer and staff hours, class information, and volunteer shifts.</p>
+    <h2 style="text-align: right; right: 40%;">All your data, in one place</h2>
+    <p style="text-align: right; right: 40%;">Administrators have easy access to all organization information. Download CSV reports of volunteer and staff hours, class information, and volunteer shifts.</p>
   </div>
 
 </body>
