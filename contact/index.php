dd11d340729602c08db7133b3f5c5159a96f717d
padillac <shinimaninima@gmail.com>
2019-07-28 13:49:42 -0500

    added about me paragraph to contact page

--- contact/index.php
+++ contact/index.php
@@ -19,11 +19,13 @@
   <div class="main-content">
 
     <h2>About:</h3>
-    <p>blablablabla</p>
+    <p>My name is Chris Padilla, I am a student at Carleton College in Minnesota.
+      I developed Dark Horse Scheduling as part of an internship with Forward Stride, an equine facilitated therapy center near Portland, Oregon.
+      Now I hope to make this service available to a wide range of equine facilities around the country. Please reach out with any questions you have!</p>
 
     <h2>Contact Me:</h2>
     <p>Email: <a href="mailto: chris.e.padilla@gmail.com">chris.e.padilla@gmail.com</a></p>
-    <p>Phone: <a href="tel:16123562341">(612)356-2341</a></p>
+    <p>Phone: <a href="tel: 16123562341">(612)356-2341</a></p>
 
   </div>
 
