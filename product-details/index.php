c665581aacbed365b4ff2cca228a3a8c639bde7b
padillac <shinimaninima@gmail.com>
2019-07-28 21:03:38 -0500

    added product details images

--- product-details/index.php
+++ product-details/index.php
@@ -15,16 +15,26 @@
   <?php include $_SERVER['DOCUMENT_ROOT'] . "/static/site-header.html"; ?>
 
   <div class="main-content">
+
     <h2 style="text-align: right;">Personalized Daily Schedules</h2>
     <p style="text-align: right; margin-left: 40%;">Staff, volunteers, and clients can simply type in their name to view their daily, personalized schedules. DHS helps avoid confusion and an excess of information, to make sure that everyone knows where they need to be.</p>
+
+
     <h2>Hour Tracking</h2>
     <p style="margin-right: 60%;">DHS gives staff and volunteers an easy way to record their work hours, anytime, from anywhere. </p>
+
+
     <h2 style="text-align: right;">Smart Scheduling with Conflict Detection</h2>
     <p style="text-align: right; margin-left: 40%;">Whenever a change to the schedule is made, DHS automatically checks every person, horse, and resource against the database to ensure that nothing is double-booked.</p>
+
     <h3 style="text-align: right;">Horse Use Limits</h3>
     <p style="text-align: right; margin-left: 40%;">Set daily and weekly work limits for each horse, DHS will help ensure that no horse is overworked.</p>
+
+
     <h2>Organization Directories</h2>
     <p style="margin-right: 60%;">Keep all organization contacts together, and easily accessible.</p>
+
+
     <h2 style="text-align: right;">All your data, in one place</h2>
     <p style="text-align: right; margin-left: 40%;">Administrators have easy access to all organization information. Download CSV reports of volunteer and staff hours, class information, and volunteer shifts.</p>
   </div>
