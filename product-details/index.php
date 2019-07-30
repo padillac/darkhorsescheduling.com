91996b1c167e1077802acef6e916259ea9de74f0
padillac <shinimaninima@gmail.com>
2019-07-30 00:15:47 -0500

    adding demo button to product details page

--- product-details/index.php
+++ product-details/index.php
@@ -71,6 +71,7 @@
     <h2 style="text-align: right;">All your data, in one place</h2>
     <p style="text-align: right; margin-left: 40%;">Administrators have easy access to all organization information. Download CSV reports of volunteer and staff hours, class information, and volunteer shifts.</p>
 
+    <br><br><br>
     <div id="home-page-nav">
       <button id="demo-button" type="button" onclick="window.location.href = 'https://demo.darkhorsescheduling.com/';">Try DHS Now!</button>
     </div>
