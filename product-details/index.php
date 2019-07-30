2948847c1fa35bec3ac4e556c52a91c52b65a0eb
padillac <shinimaninima@gmail.com>
2019-07-29 23:02:44 -0500

    adding images to product details page

--- product-details/index.php
+++ product-details/index.php
@@ -36,14 +36,16 @@
 
 
     <br><br><br><br><br><br><br><br><br><br>
-    <div class="screenshot screenshot-left">
-      <img src="/images/product-details/conflict-sample.png" alt="Example of a scheduling conflict being detected and reported.">
-    </div>
     <h2 style="text-align: right;">Smart Scheduling with Conflict Detection</h2>
     <p style="text-align: right; margin-left: 40%;">Whenever a change to the schedule is made, DHS automatically checks every person, horse, and resource against the database to ensure that nothing is double-booked.</p>
+    <div class="screenshot screenshot-right">
+      <img src="/images/product-details/conflict-sample.png" alt="Example of a scheduling conflict being detected and reported.">
+    </div>
+
 
     <h3 style="text-align: right;">Horse Use Limits</h3>
     <p style="text-align: right; margin-left: 40%;">Set daily and weekly work limits for each horse, DHS will help ensure that no horse is overworked.</p>
+
 
 
     <h2>Organization Directories</h2>
