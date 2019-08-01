0de1130765f228c57379f3cf5100635c0515650f
padillac <shinimaninima@gmail.com>
2019-08-01 17:26:20 -0500

    re-thinking intro paragraph

--- index.php
+++ index.php
@@ -12,10 +12,11 @@
 	<header id="main-page-header">Dark Horse Scheduling</header>
 
   <div class="intro-paragraph">
-    <p>Do you need a simpler and more effective way to manage daily classes, appointments, and volunteer shifts?</p>
-    <p>Volunteers need easy and clutter-free access to their shift information so they can prepare for their days,
-      as well as a quick, accessible way to report their hours.
-      Staff need a place where they can plan sessions, keep progress notes and attendance records, and report hours.
+    <p>Do you need a simpler and more effective way to manage daily lessons, appointments, and volunteer shifts?</p>
+    <p>Managing an equine facility is no easy task.
+      Volunteers need easy and clutter-free access to their shift information so they can prepare for their days,
+      as well as a quick, accessible way to report their hours on the go.
+      Instructors need a place where they can plan sessions, keep notes and attendance records, and report work hours.
       Coordinators need to be able to quickly view and change the entire season's schedule at a glance.
       DHS keeps your entire schedule on one real-time database, so everyone's always up to date. </p>
     <p>Dark Horse Scheduling is a user-friendly schedule management solution tailored specifically to equine facilities.
