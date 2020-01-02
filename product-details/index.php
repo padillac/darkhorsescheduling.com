4d2d4c8b8c8308f359c88f66e2b17dd04ad7b0d5
Chris Padilla <shinimaninima@gmail.com>
2020-01-01 19:30:42 -0500

    fixed styles for product details page

--- product-details/index.php
+++ product-details/index.php
@@ -36,7 +36,7 @@
 
 
 
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <h2>Hour Tracking</h2>
     <p style="margin-right: 60%;">DHS gives staff and volunteers an easy way to record their work hours, anytime, from anywhere. </p>
     <div class="screenshot screenshot-right">
@@ -45,7 +45,7 @@
     
 
 
-    <br><br><br><br><br>
+    <br><br><br><br><br><br><br>
     <h2 style="text-align: right;">Smart Scheduling with Conflict Detection</h2>
     <p style="text-align: right; margin-left: 40%;">Whenever a change to the schedule is made, DHS automatically checks every person, horse, and resource against the database to ensure that nothing is double-booked.</p>
     <div class="screenshot screenshot-right">
